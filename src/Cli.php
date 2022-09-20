593ae8a293d413649ffa5e7d0426b8a62e9c466a
Gennadiy Kim <gkim@cs-cart.com>
2022-09-20 23:21:51 +0500

    Add wp-cli/php-cli-tools package

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace BrainGames;
+
+use function cli\line;
+use function cli\prompt;
+
+function welcome()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
